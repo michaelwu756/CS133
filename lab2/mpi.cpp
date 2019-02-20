0cfe8dbe1a237d003d50bca05d0a670e782571e7
Michael Wu <cheeserules43@gmail.com>
2019-02-19 23:01:32 -0800

    removed comments

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -1,11 +1,7 @@
-// Header inclusions, if any...
-
 #include <mpi.h>
 #include <cstring>
 #include <stdlib.h>
 #include "../lab1/gemm.h"
-
-// Using declarations, if any...
 
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
